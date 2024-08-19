771a191773be39c840b5ca66ee14ac0ca465961b
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2024-08-19 09:55:59 -0300

    script.js

--- modulotecnicascomputacionais/IAluzia-main/IAluzia/script.js
+++ modulotecnicascomputacionais/IAluzia-main/IAluzia/script.js
@@ -4,44 +4,58 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
-
 const perguntas = [
     {
-        enunciado:"Todos os lugares do mundo tem acesso a cuidados para a saúde?",
-        alternativas:[
-           "pois em cidades pequenas normalmente tem menos acesso aos medicamentos e atendimentos necessários.",
-           "a tribo entende que a reprodução é necessario e nao deve ser contida.",
-            
+        enunciado: "Todos os lugares do mundo tem acesso a cuidados para a saúde?",
+        alternativas: [
+            {
+                texto: "não.",
+                afirmação: "pois em cidades pequenas normalmente tem menos acesso aos medicamentos e atendimentos necessários.",
+                ""
+            },
+            {
+                texto: "nao",
+                afirmação: "a tribo entende que a reprodução é necessario e nao deve ser contida."
+            }
         ]
     },
-]
-
     {
-        enunciado:"Usar roupas enfeitadas na festa junina é necessário?",
-        alternativas;[
-            "Depende da festa e oque ela esta pedindo como traje.",
-            "a maioria das  festas é sim necessário ir com roupas enfeitadas",
+        enunciado: "Usar roupas enfeitadas na festa junina é necessário?",
+
+        alternativas: [
+            {.
+                texto: "sim",
+                afirmacao: "Depende da festa e oque ela esta pedindo como traje."
+            },
+            {
+                texto: "Em outras nao tanto.",
+                afirmacao: "mas a maioria das  festas é sim necessário ir com roupas enfeitadas"
+
+            }
+        ]
+    },
+    {
+        enunciado: "Todos os lixos recicláveis devem ser reciclados corretamente?",
+
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "para ter um meio ambiente mais limpo."
+            },
+            {
+                texto: "para ter um meio ambiente melhor sim",
+                afirmacao: "sem lixo acumulado."
+            }
         ]
     }
-
-
-    {
-        enunciado:"Todos os lixos recicláveis devem ser reciclados corretamente?",
-        alternativas;[
-            "Sim, para ter um meio ambiente mais limpo.",
-            "se for reciclável devemos sim reciclar para ajudar o lixeiros na coleta.",
-        ]
-    }
-
+];
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-
- function  mostraPerguntas(){
-    if(atual >=perguntas.length){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
@@ -50,30 +64,38 @@ let historiaFinal = "";
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
- }
+}
 
- function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
- }
+}
 
- function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
- }
+}
 
- function mostraResultado() {
+function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
- }
-  
- mostraPergunta();
+}
+
+function aleatoria(lista){
+    const posicao = Math.random()*lista.length;
+}
+
+mostraPergunta();
+
+
+	
+
 
 
